ac45770c988aa370a9e6813a537cdd55b48de5f3
Baljeesi <150318997+Baljeesi@users.noreply.github.com>
2025-10-27 15:06:19 +0530

    build.sh

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ cd /home/ubuntu/Course-section8-main/
 npm install
 npm run build
 chmod 744 push_docker_image.sh
-/home/ubuntu/sagar2/push_docker_image.sh
+/home/ubuntu/Course-section8-main/push_docker_image.sh
